cebfa44f20be23a997ab4051775c6efef5a3f1a0
3º ETIM - Desenvolvimento de Sistemas <ds-etim-3@servidor.acad>
2024-08-13 15:26:53 -0300

    v4

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const filterButtons = document.querySelectorAll('.filter-button');
+const portfolioItems = document.querySelectorAll('.portfolio-item');
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        filterButtons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+        const filter = button.getAttribute('data-filter');
+
+        portfolioItems.forEach(item => {
+            if (filter === 'all' || item.getAttribute('data-category') === filter) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    });
+});
